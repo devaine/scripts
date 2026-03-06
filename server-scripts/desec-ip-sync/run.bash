107f8b86262e268d181bf803732ff341616746f5
devaine <devaine@devdoes.work>
2026-03-06 12:49:07 -0600

    build(Dockerfile): adding a ENV on the virtual environment might do it

--- server-scripts/desec-ip-sync/run.bash
+++ server-scripts/desec-ip-sync/run.bash
@@ -9,7 +9,7 @@ fi
 if [ ! -d venv ]; then
 	python3 -m venv venv
 	source venv/bin/activate
-	pip install -U python-dotenv requests
+	pip install -U -r requirements.txt
 	pip install -U pip # Update pip to the latest version
 else
 	source venv/bin/activate
